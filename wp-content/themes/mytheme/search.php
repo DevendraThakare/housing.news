0e9c493377311a0e5d864c118aaf6788dba9d118
devendra thakare <devendrathakre26@gmail.com>
2016-03-17 13:25:51 +0530

    Search page modifications

--- wp-content/themes/mytheme/search.php
+++ wp-content/themes/mytheme/search.php
@@ -8,9 +8,9 @@
  */
 
 get_header(); ?>
-
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div id="page-container" class="clear">
+		<div id="primary" class="content-area">
+			<div id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
@@ -21,14 +21,7 @@ get_header(); ?>
 			<?php
 			// Start the loop.
 			while ( have_posts() ) : the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
 				get_template_part( 'template-parts/content', 'search' );
-
 			// End the loop.
 			endwhile;
 
@@ -46,8 +39,8 @@ get_header(); ?>
 		endif;
 		?>
 
-		</main><!-- .site-main -->
-	</section><!-- .content-area -->
-
-<?php get_sidebar(); ?>
+		</div><!-- .site-main -->
+	</div><!-- .content-area -->
+	<?php get_sidebar(); ?>
+</div>
 <?php get_footer(); ?>
--- wp-content/themes/mytheme/template-parts/content-search.php
+++ wp-content/themes/mytheme/template-parts/content-search.php
@@ -9,18 +9,41 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	</header><!-- .entry-header -->
+	<?php post_thumbnail(); ?>
+	<div class="post-content-wrap">
+		<header class="entry-header">
+			<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
+				<span class="sticky-post"><?php _e( 'Featured', 'twentysixteen' ); ?></span>
+			<?php endif; ?>
+			<div class="post-meta">
+				<?php post_meta(); ?>
+			</div>
+			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		</header><!-- .entry-header -->
 
-	<?php twentysixteen_post_thumbnail(); ?>
 
-	<?php twentysixteen_excerpt(); ?>
 
-	<?php if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-content">
+			<?php
+				/* translators: %s: Name of current post */
+				echo post_excerpt();
+				// the_content( sprintf(
+				// 	__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+				// 	get_the_title()
+				// ) );
+
+				wp_link_pages( array(
+					'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
+					'after'       => '</div>',
+					'link_before' => '<span>',
+					'link_after'  => '</span>',
+					'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
+					'separator'   => '<span class="screen-reader-text">, </span>',
+				) );
+			?>
+		</div><!-- .entry-content -->
 
 		<footer class="entry-footer">
-			<?php twentysixteen_entry_meta(); ?>
 			<?php
 				edit_post_link(
 					sprintf(
@@ -33,21 +56,6 @@
 				);
 			?>
 		</footer><!-- .entry-footer -->
-
-	<?php else : ?>
-
-		<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Name of current post */
-					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-					get_the_title()
-				),
-				'<footer class="entry-footer"><span class="edit-link">',
-				'</span></footer><!-- .entry-footer -->'
-			);
-		?>
-
-	<?php endif; ?>
+	</div>
 </article><!-- #post-## -->
 
