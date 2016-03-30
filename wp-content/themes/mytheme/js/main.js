e98111b050b92b2be48dd0c0533636e43932dc87
devendra thakare <devendrathakre26@gmail.com>
2016-03-30 16:29:41 +0530

    header transperent

--- wp-content/themes/mytheme/js/main.js
+++ wp-content/themes/mytheme/js/main.js
@@ -6,5 +6,47 @@
 		"offset": 580,
 		"tolerance": 5});
 		headroom.init(); 
+
+		headroom.offset = 5000;
+
+		tout = null;
+		offset_arr = {
+			'default': 580,
+			'1024': 500,
+			'768': 400,
+			'480': 240,
+			'375': 240,
+			'320': 200
+		}
+
+		$(window).resize(function() {
+			if(tout){
+				clearTimeout(tout);
+			}
+			tout = setTimeout(function() {
+				width = $(window).width();
+				offset = 580
+				switch (true) {
+					case width <= 1024:
+						offset = 500;
+						break;
+					case width<=768:
+						offset = 400;
+						break;
+					case width<=480:
+						offset = 240;
+						break;
+					case width<=320:
+						offset = 200;
+						break;                  
+					default:
+						offset = 580;
+						break; 
+				}
+				headroom.offset = offset;
+			}, 400);
+		});
+
+		$(window).trigger('resize');
 	});
 })(jQuery );
